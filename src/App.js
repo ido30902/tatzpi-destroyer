dadea71499c362ed0491c694b4815804390fa41c
ido30902 <idobarabi@gmail.com>
2022-02-27 23:58:40 +0200

    added inputs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
 
+var result = 0;
+var weight = 0;
+var height = 0;
+
 function App() {
+  
+  const calculate = (e) => {
+    
+    result = Math.round(Math.pow(e.value.height, Math.E) + (e.value.weight/100) + 1);
+
+    console.log('works' + result);
+
+    //var weight = document.getElementsByClassName("weight").value;
+    //var height = document.getElementsByClassName("height").value;
+    
+    document.getElementsByClassName("result").innerHTML= result;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <fieldset>
+         <label>
+           <p>Weight</p>
+           <input value={weight} className="weight" />
+         </label>
+         <label>
+           <p>Height</p>
+           <input value={height} className="height" />
+         </label>
+       </fieldset>
+       <button type="submit" onSubmit={calculate}>Submit</button>
     </div>
   );
 }
+
 
 export default App;
