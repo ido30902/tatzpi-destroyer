99ed90367d4b859d7e39c67ec3e362d6752f1ab1
ido30902 <idobarabi@gmail.com>
2022-02-28 11:18:38 +0200

    alpha

--- src/App.js
+++ src/App.js
@@ -1,36 +1,67 @@
+import { FormControl } from '@mui/material';
+import { Button, TextField } from '@mui/material';
+import React from 'react';
 import './App.css';
-
-var result = 0;
-var weight = 0;
-var height = 0;
 
 function App() {
   
-  const calculate = (e) => {
+  const calculate = () => {
     
-    result = Math.round(Math.pow(e.value.height, Math.E) + (e.value.weight/100) + 1);
+    result = Math.round(Math.pow(height / 100, Math.E) + (weight / age) + age / 10); 
 
-    console.log('works' + result);
+    setResult(result);
 
-    //var weight = document.getElementsByClassName("weight").value;
-    //var height = document.getElementsByClassName("height").value;
-    
-    document.getElementsByClassName("result").innerHTML= result;
+    console.log('works ' + result);
+
   }
+
+  const [height, setHeight] = React.useState('');
+  const [weight, setWeight] = React.useState('');
+  const [age, setAge] = React.useState('');
+  var [result, setResult] = React.useState('');
+
+  const handleHeightChange = (e) => {
+    setHeight(e.target.value);
+  };
+
+  const handleWeightChange = (e) => {
+    setWeight(e.target.value);
+  };
+
+  const handleAgeChange = (e) => {
+    setAge(e.target.value);
+  };
+
+  const formStyle = {
+    padding: '40px',
+    textAlign: 'center',
+    margin: '15px'
+  };
+
+  const buttonStyle = {
+    margin: '15px',
+    borderRadius: '20px',
+    border: '2px solid black',
+    color: 'white',
+    backgroundColor: 'cyan'
+  };
+
+  const inputStyle = {
+    margin: '7px',
+    textAlign: 'center',
+  };
 
   return (
     <div className="App">
-      <fieldset>
-         <label>
-           <p>Weight</p>
-           <input value={weight} className="weight" />
-         </label>
-         <label>
-           <p>Height</p>
-           <input value={height} className="height" />
-         </label>
-       </fieldset>
-       <button type="submit" onSubmit={calculate}>Submit</button>
+      <FormControl style={formStyle}>
+        <TextField style={inputStyle} type="number" value={height} onChange={handleHeightChange} placeholder="Height"/>
+        <TextField style={inputStyle} type="number" value={weight} onChange={handleWeightChange} placeholder="Weight"/>
+        <TextField style={inputStyle} type="number" value={age} onChange={handleAgeChange} placeholder="Age"/>
+        <Button style={buttonStyle} onClick={calculate}>Calculate</Button>
+      </FormControl>
+
+      <h1>You can Knock-Off {result} Tatzpi before you die</h1>
+
     </div>
   );
 }
